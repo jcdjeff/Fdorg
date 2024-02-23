1600a8f631385c6129824c497db1e3c890acdb66
webappjeff <design@webappjeff.com>
2024-02-23 16:08:19 -0500

    Type Safety

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,7 +6,8 @@ import ProductListItem from '@/src/components/ProductListItem';
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[1]}/>
+      <ProductListItem product={products[2]} />
+      <ProductListItem product={products[5]}/>
     </View>
   );
 };
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,39 +2,44 @@ import { Image, StyleSheet } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
 import products from '@/assets/data/products';
+import { Product } from '../types'
 
+export const defaultImg = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
+type ProductListItemProps = {
+    product: Product;
+}
 
-const ProductListItem = ({product}) => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
-      <Text style={styles.title}>{product.name} </Text>
-      <Text style={styles.price}>$ {product.price}</Text>
-    </View>
-  );
+const ProductListItem = ({product}: ProductListItemProps) => {
+return (
+<View style={styles.container}>
+    <Image style={styles.image} source={{ uri: product.image || defaultImg }} />
+    <Text style={styles.title}>{product.name} </Text>
+    <Text style={styles.price}>$ {product.price}</Text>
+</View>
+);
 };
 
 export default ProductListItem; 
-  
+
 const styles = StyleSheet.create({
-  container: {
+    container: {
     backgroundColor: '#fff',
     borderRadius: 20, 
     padding: 10,
-  },
-  image: {
+    },
+    image: {
     width: "100%",
     aspectRatio: 1,
-  },
-  title: {
+    },
+    title: {
     fontSize: 20,
     fontWeight: 'bold',
     marginVertical: 10, 
-    
-  },
-  price: {
+
+    },
+    price: {
     color: Colors.light.tint, 
-  }
+    }
 
 });
